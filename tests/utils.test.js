d502353ddd28c6bdea7cfb18c9771dddedbd77d9
ilikecoffee18 <cherish.kim@students.makeschool.com>
2019-02-14 15:35:59 -0800

    added classwork from 2/13/19

--- tests/utils.test.js
+++ tests/utils.test.js
@@ -6,6 +6,8 @@ const expect = chai.expect;
 // ========================================================
 // NOTE: https://mochajs.org/#arrow-functions
 // Passing arrow functions (“lambdas”) to Mocha is discouraged.
+// MUST REMEMBER THIS
+// USING FAT ARROWS WHEN USING MOCHA AND CHAI WILL MAKE TESTS FAIL WHEN YOUR CODE IS PERFECTLY FINE
 // Lambdas lexically bind this and cannot access the Mocha context.
 // ========================================================
 
@@ -20,9 +22,23 @@ it("should say hello", function() {
 // Level 1 Challenges
 // ========================================================
 
-it("should return the area of a 5 by 6 rectangle");
+it("should return the area of a 5 by 6 rectangle", function() {
+  const rect = utils.area(5, 6);
+  expect(rect).to.be.a("number");
+  expect(rect).to.equal(30);
+});
 
-it("should return the are of a circle of radius 5");
+it("should return the perimeter of a 5 by 6 rectangle", function() {
+  const rectPerim = utils.perimeter(5, 6);
+  expect(rectPerim).to.be.a("number");
+  expect(rectPerim).to.equal(22);
+});
+
+it("should return the area of a circle of radius 5", function() {
+  const circle = utils.circleArea(5);
+  expect(circle).to.be.a("number");
+  expect(circle).to.equal(Math.PI * 25);
+});
 
 // ========================================================
 // Level 2 Challenges
@@ -32,15 +48,45 @@ it("should return the are of a circle of radius 5");
 // tests eventually.
 // ========================================================
 
-it("Should create a new (object) Item with name and price");
 
-it("Should return an array containing all items in cart");
+const cart = {items: [ {name: "app", price: 0}, {name: "bat", price: 1}, {name: "cat", price: 2} ]}
 
-it("Should add a new item to the shopping cart");
+it("Should create a new (object) Item with name and price", function() {
+  const name = cart.items[0].name;
+  const price = cart.items[0].price;
+  expect(name).to.be.a("string");
+  expect(price).to.be.a("number");
+  expect(name).to.equal("app");
+  expect(price).to.equal(0);
+});
 
-it("Should return the number of items in the cart");
+it("Should return an array containing all items in cart", function() {
+  const urStuff = cart.items;
+  expect(urStuff).to.be.a("array");
+});
 
-it("Should remove items from cart");
+it("Should add a new item to the shopping cart", function() {
+  cart.items.push({name: 'day', price: 3});
+  const thingName = cart.items[3].name;
+  const thingPrice = cart.items[3].price;
+  expect(thingName).to.be.a("string");
+  expect(thingName).to.equal("day");
+  expect(thingPrice).to.be.a("number");
+  expect(thingPrice).to.equal(3);
+});
+
+it("Should return the number of items in the cart", function() {
+  const cartQuant = cart.items.length;
+  expect(cartQuant).to.be.a("number");
+  expect(cartQuant).to.equal(4);
+});
+
+it("Should remove items from cart", function() {
+  cart.items.pop();
+  const shrunkCart = cart.items.length;
+  expect(shrunkCart).to.be.a("number");
+  expect(shrunkCart).to.equal(3);
+});
 
 // ========================================================
 // Stretch Challenges
